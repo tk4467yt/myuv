c32b22484e27a133364a781a730d5979020a5908
qin <xf.xu@wowtech-inc.com>
2018-03-30 10:35:31 +0800

    tc

--- codes/helloworld/src/helloworld.c
+++ codes/helloworld/src/helloworld.c
@@ -2,16 +2,37 @@
 #include <stdlib.h>
 #include <uv.h>
 
+uint64_t counter = 0;
+void wait_for_a_while(uv_idle_t *idler)
+{
+	++counter;
+
+	if (counter >= 10e6) {
+		printf("idler stop\n");
+		uv_idle_stop(idler);
+	}
+}
 int main()
 {
-	uv_loop_t *loop = malloc(sizeof(uv_loop_t));
-	uv_loop_init(loop);
+	uv_idle_t idler;
 
-	printf("Now quitting.\n");
-	uv_run(loop, UV_RUN_DEFAULT);
+	uv_idle_init(uv_default_loop(), &idler);
+	uv_idle_start(&idler, wait_for_a_while);
 
-	uv_loop_close(loop);
-	free(loop);
+	printf("Idling\n");
+	uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 
+	uv_loop_close(uv_default_loop());
 	return 0;
+
+	// uv_loop_t *loop = malloc(sizeof(uv_loop_t));
+	// uv_loop_init(loop);
+
+	// printf("Now quitting.\n");
+	// uv_run(loop, UV_RUN_DEFAULT);
+
+	// uv_loop_close(loop);
+	// free(loop);
+
+	// return 0;
 }
