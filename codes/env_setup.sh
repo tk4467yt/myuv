24923dc9f7d101752631e0de7e114b7cf1abd5e7
qin <xf.xu@wowtech-inc.com>
2018-04-08 10:22:43 +0800

    tc

--- codes/env_setup.sh
+++ codes/env_setup.sh
@@ -6,7 +6,7 @@ docker network create dnet1
 docker run -d --rm --hostname myrabbit --name myrabbitd --network dnet1 -p 5672:5672 -p 5671:5671 rabbitmq
 
 #run rabbitmq consumer
-docker run -itd --rm --network dnet1 --name a1  alpine
+docker run -it --rm --network dnet1 xuqin:rabbitmq_receiver_python
 
 #run tcpserver
 docker run -itd --rm --network dnet1 --name tcpserver -p 7000:7000  xuqin:uvtcpserver
