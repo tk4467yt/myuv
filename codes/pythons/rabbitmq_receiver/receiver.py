edfc2c69be649ef557ff9e642e27a82ec3b11fff
qin <brokenmoon@126.com>
2018-04-08 20:59:36 +0800

    tc

--- codes/pythons/rabbitmq_receiver/receiver.py
+++ codes/pythons/rabbitmq_receiver/receiver.py
@@ -1,21 +1,36 @@
+import time
 import pika
 
 #run with docker
 #docker run -it  --rm --network dnet1 xuqin:rabbitmq_receiver_python
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('myrabbitd'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='myrabbitd'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.exchange_declare(exchange='direct_logs',
+                         exchange_type='direct')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+severities = ['info']
+
+if not severities:
+    sys.stderr.write("Usage: %s [info] [warning] [error]\n" % sys.argv[0])
+    sys.exit(1)
+
+for severity in severities:
+    channel.queue_bind(exchange='direct_logs',
+                       queue=queue_name,
+                       routing_key=severity)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
 
 channel.basic_consume(callback,
-                      queue='hello',
+                      queue=queue_name,
                       no_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
-
-connection.close()
